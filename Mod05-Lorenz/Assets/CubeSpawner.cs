eaa1adefd0104f2d5e5c9c4376be4ef8336f6f80
juliaclorenz <julialorenz17@icloud.com>
2023-02-28 13:42:46 -0600

    :)
    CONFETTI CUBES

---
+++ Mod05-Lorenz/Assets/CubeSpawner.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+
+{
+    public GameObject cubePrefabVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject cubeGO = Instantiate<GameObject>(cubePrefabVar);
+        Material mat = cubeGO.GetComponent<Renderer>().material;
+        mat.color = UnityEngine.Random.ColorHSV(0, 1, 0.5f, 1, 0.75f, 1);
+    }
+}
